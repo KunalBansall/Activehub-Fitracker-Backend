1025f800c9f9ad1e408fd48f655deb335ce2f1aa
email <obansal.kunalo@gmail.com>
2024-12-06 14:48:45 +0530

    Added Atteendence Route

--- backend/src/routes/attendance.js
+++ backend/src/routes/attendance.js
@@ -9,5 +9,7 @@ router.use(authenticateAdmin);
 router.post('/entry/:memberId', attendanceController.recordEntry);
 router.post('/exit/:memberId', attendanceController.recordExit);
 router.get('/today', attendanceController.getTodayAttendance);
+router.get('/history/:memberId', attendanceController.getMemberAttendanceHistory);
+
 
 module.exports = router;
